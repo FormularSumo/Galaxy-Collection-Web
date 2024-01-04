24930709cc75860af22af1f9ce06a2c5e9544e19
FormularSumo2 <36278229+FormularSumo@users.noreply.github.com>
2024-01-04 13:43:39 +0000

    Test changing cache

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/',
+      '/Star-Wars-Galaxy-Collection-Web/',
     ]);
   })());
 });
