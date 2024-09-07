6d6798a34ed3c09d1939a108cab4741ef7750929
FormularSumo <chewy.h@live.co.uk>
2024-09-07 22:12:03 +0200

    Remove old reloading code, test new game text

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"2ad603fc-d12f-4289-b69d-9873e336f4d5","remote_package_size":72818274,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":72818274,"audio":false}]});
+  loadPackage({"package_uuid":"0e642a32-e227-44dc-985e-281120ff1ac4","remote_package_size":72818279,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":72818279,"audio":false}]});
 
 })();
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `0.12.0.17`; // stable.pre-release.minor.web
+const CACHE_NAME = `0.12.0.18`; // stable.pre-release.minor.web
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
@@ -31,13 +31,6 @@ self.addEventListener("activate", (e) => { //Delete old caches
       );
     }),
   );
-  // location.reload()  
-  // for (const client of clients.matchAll()) {
-  //   // Reload the page
-  //   client.navigate(client.url);
-  // }
-  // window.location.reload();
-  // client.navigate(client.url);
 });
 
 self.addEventListener('fetch', event => {
