13280ec941e5a61ad581ad555b69adcf1f86883c
FormularSumo <chewy.h@live.co.uk>
2024-09-07 21:46:07 +0200

    Test reloading site after new service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `0.12.0.13`; // stable.pre-release.minor.web
+const CACHE_NAME = `0.12.0.14`; // stable.pre-release.minor.web
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
@@ -15,6 +15,8 @@ self.addEventListener('install', event => {
       'game.js'
     ]);
     cache.delete('game.data')
+    window.location.reload();
+    // client.navigate(client.url);
     })());
 });
 
