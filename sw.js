999e17a349354a5de6a7013137c1801caf180d7b
FormularSumo <chewy.h@live.co.uk>
2024-09-07 13:49:07 +0200

    Revert "Update scope of Cache/SW"
    This reverts commit 9d5d34819ad4ca4a6a10769ccaa8c9f542f35c74.
    
    Turns out it was because Github Pages needs different service worker scope to localhost

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `0.12.0.1`; // stable.pre-release.minor.web
+const CACHE_NAME = `v3`;
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
@@ -6,7 +6,7 @@ self.addEventListener('install', event => {
     const cache = await caches.open(CACHE_NAME);
     console.log("Content downloading")
     cache.addAll([
-      '/',
+      '/Star-Wars-Galaxy-Collection-Web/',
     ]);
     return this.skipWaiting();
   })());
