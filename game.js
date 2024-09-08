a68f11151846be5e41a321307345629e50826890
FormularSumo <chewy.h@live.co.uk>
2024-09-08 17:46:20 +0200

    Move remove button up
    I'm changing Service Worker cache version to 0 as this **should** be final 0.12 version now. There'll still be other versions shortly after likely to fix web specific things, but not more changes to the main repo/game code as part of 0.12

--- game.js
+++ game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid":"055fbc17-d8f5-4b26-9a06-fa409af8fae7","remote_package_size":72818261,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":72818261,"audio":false}]});
+  loadPackage({"package_uuid":"9c9ce071-a4c5-4583-ac6a-fbf4f9104278","remote_package_size":72818261,"files":[{"filename":"/game.love","crunched":0,"start":0,"end":72818261,"audio":false}]});
 
 })();
