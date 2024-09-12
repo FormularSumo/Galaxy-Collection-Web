227f5ff1c7b56a1690319aef1e059fac79f3cbf9
FormularSumo <chewy.h@live.co.uk>
2024-09-12 20:01:06 +0200

    Remove/comment out old test code

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `0.12.0.4`; // stable.pre-release.minor.web
+const CACHE_NAME = `0.12.0.5`; // stable.pre-release.minor.web
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
@@ -6,7 +6,7 @@ self.addEventListener('install', event => {
 
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
-    console.log("Content downloading")
+    // console.log("Content downloading")
     cache.addAll([
         // '/'
       'index.html',
