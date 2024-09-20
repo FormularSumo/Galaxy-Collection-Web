5d67d6637556dffae5c355e56264ee7bbfd5e06c
FormularSumo <chewy.h@live.co.uk>
2024-09-20 20:30:28 +0200

    Go back to including resizing on iOS - final 0.12.1 release
    It did in fact just makes things worse, not sure why/how it helped before

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = `0.12.0.15`; // stable.pre-release.minor.web
+const CACHE_NAME = `0.12.1.0`; // stable.pre-release.minor.web
 
 // Use the install event to pre-cache all initial resources.
 self.addEventListener('install', event => {
